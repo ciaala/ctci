02f2866e7f00320b81099c3fc3d3bc0aa7dd8639
crypt <francesco.fiduccia@gmail.com>
2016-06-06 00:11:10 +0200

    chapter 10          Exercise1 updated the merging code

--- src/chapter10/exercise1/SortedMerge.cpp
+++ src/chapter10/exercise1/SortedMerge.cpp
@@ -14,8 +14,9 @@ namespace exercise10_1 {
         queue<int> queue;
         size_t i_a = 0, i_b = 0;
         while (i_b < size_b) {
-            queue.emplace(a[i_a]);
-            if (queue.size() > 0 && (queue.front() <= b[i_b])) {
+            if (i_a < size_a) queue.emplace(a[i_a]);
+
+            if (!queue.empty() && queue.front() <= b[i_b]) {
                 a[i_a] = queue.front();
                 queue.pop();
             } else {
@@ -25,7 +26,7 @@ namespace exercise10_1 {
             i_a++;
         }
 
-        while (queue.size()) {
+        while (!queue.empty()) {
             a[i_a] = queue.front();
             queue.pop();
             i_a++;
@@ -34,15 +35,22 @@ namespace exercise10_1 {
     }
 
     void main() {
-        int a[] = {0, 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 31, 37, 41, 43, 47, 53, 57, 59, 999, 999, 999, 999, 999, 999,
-                   999};
-        int b[] = {1, 8, 9, 10, 60, 62};
+        int a[] = {0, 1, 2,
+                   3, 5, 7,
+                   11, 13, 17,
+                   19, 23, 31,
+                   37, 41, 43,
+                   47, 53, 57,
+                   59, 999, 999,
+                   999, 999, 999,
+                   999, 999};
+        int b[] = {1, 8, 9, 58, 60, 62};
         SortedMerge::sortedMerge(a, 19, b);
         for (int i = 0;
              i < 26;
              i++) {
             cout << a[i] << ' ';
         }
-        cout << endl;
+        cout << "." << endl;
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ extern int main_chapter5(const int, const char **);
 
 extern int main_chapter10(const int, const char **);
 int main(const int argc, const char **argv) {
-    main_chapter1(argc, argv);
-    main_chapter5(argc, argv);
+    //main_chapter1(argc, argv);
+    //main_chapter5(argc, argv);
 
     main_chapter10(argc, argv);
     std::cout << "Exiting" << std::endl;
