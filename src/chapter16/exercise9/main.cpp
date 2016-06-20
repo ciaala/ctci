aad485e6d0b8884a3b0d7275f37fc5ce0a223216
Francesco Fiduccia <francesco.fiduccia@know.eu>
2016-06-20 22:10:12 +0200

    16.9 Fixed

--- src/chapter16/exercise9/main.cpp
+++ src/chapter16/exercise9/main.cpp
@@ -13,7 +13,7 @@ namespace exercise16_9 {
     class Mint {
         const int value;
 
-        static int negative(int a) {
+        static int naiveNegative(int a) {
             int newSign = a > 0 ? -1 : 1;
             int result = 0;
             while (a != 0) {
@@ -21,6 +21,32 @@ namespace exercise16_9 {
                 a += newSign;
             }
             return result;
+        }
+
+        static int negative(int a) {
+            int newSign = a > 0 ? -1 : 1;
+            int result = 0;
+            int delta = newSign;
+            int counter = 0;
+            while (a != 0) {
+                result += delta;
+                a += delta;
+                delta += delta;
+                bool signChange = (a + delta) > 0 != (a > 0);
+                if (a + delta != 0 && signChange) {
+                    delta = newSign;
+                }
+                counter++;
+            }
+            return result;
+        }
+
+        static int abs(int v) {
+            if (v < 0) {
+                return negative(v);
+            } else {
+                return v;
+            }
         }
 
     public:
@@ -35,10 +61,19 @@ namespace exercise16_9 {
 
         Mint operator*(Mint &other) {
             int result = 0;
-            for (int i = other.value; i > 0; i--) {
+            int signDifferent = (other.value > 0 != this->value > 0);
+            int operand2;
+            if (signDifferent)
+                operand2 = negative(other.value);
+            else
+                operand2 = other.value;
+            for (int i = operand2; i > 0; i--) {
                 result += value;
             }
-            return result;
+            if (signDifferent)
+                return negative(result);
+            else
+                return result;
         }
 
         Mint operator-(Mint &other) {
@@ -49,12 +84,20 @@ namespace exercise16_9 {
 
         Mint operator/(Mint &other) {
             int result = 0;
-            int temp = other.value;
-            while (temp < value) {
-                temp += other.value;
+
+            int signDifferent(other.value > 0 != this->value > 0);
+            int operand2 = abs(other.value);
+            int operand1 = abs(this->value);
+            int temp = operand2;
+
+            while (temp < operand1) {
+                temp += operand2;
                 result += 1;
             }
-            return result;
+            if (signDifferent)
+                return negative(result);
+            else
+                return result;
         }
 
         friend ostream &operator<<(ostream &ost, const Mint &mint) {
@@ -65,8 +108,8 @@ namespace exercise16_9 {
     };
 
     void main() {
-        Mint m5 = Mint(5);
-        Mint m4 = Mint(4);
+        Mint m5 = Mint(50000);
+        Mint m4 = Mint(-20000);
 
         cout << "+: " << m5 + m4 << endl;
         cout << "-: " << m5 - m4 << endl;
