294bada050efd7b3671a52ebd28dcfdb317fb92c
Francesco Fiduccia <francesco.fiduccia@know.eu>
2016-06-20 21:00:52 +0200

    16.9 Completed

--- src/chapter16/exercise9/README.md
+++ src/chapter16/exercise9/README.md
@@ -1,1 +1,8 @@
-#16.
+#16.9 Operations
+Write methods to implement the multiply, subtract and divide operations for integers.
+The results of all these are integers. Use only the add operator.
+
+_#572_
+_#600_
+_#613_
+_#648_
--- src/chapter16/exercise9/main.cpp
+++ src/chapter16/exercise9/main.cpp
@@ -4,14 +4,74 @@
 
 #include <iostream>
 #include <ctime>
+#include <climits>
 
 using namespace std;
 
 namespace exercise16_9 {
 
+    class Mint {
+        const int value;
 
+        static int negative(int a) {
+            int newSign = a > 0 ? -1 : 1;
+            int result = 0;
+            while (a != 0) {
+                result += newSign;
+                a += newSign;
+            }
+            return result;
+        }
+
+    public:
+        Mint(int value) : value(value) {
+
+        }
+
+        Mint operator+(Mint &other) {
+            int result = this->value + other.value;
+            return result;
+        }
+
+        Mint operator*(Mint &other) {
+            int result = 0;
+            for (int i = other.value; i > 0; i--) {
+                result += value;
+            }
+            return result;
+        }
+
+        Mint operator-(Mint &other) {
+            int operand = Mint::negative(other.value);
+            int result = this->value + operand;
+            return result;
+        }
+
+        Mint operator/(Mint &other) {
+            int result = 0;
+            int temp = other.value;
+            while (temp < value) {
+                temp += other.value;
+                result += 1;
+            }
+            return result;
+        }
+
+        friend ostream &operator<<(ostream &ost, const Mint &mint) {
+            ost << mint.value << "m ";
+            return ost;
+        }
+
+    };
 
     void main() {
+        Mint m5 = Mint(5);
+        Mint m4 = Mint(4);
+
+        cout << "+: " << m5 + m4 << endl;
+        cout << "-: " << m5 - m4 << endl;
+        cout << "/: " << m5 / m4 << endl;
+        cout << "*: " << m5 * m4 << endl;
     }
 
 }
