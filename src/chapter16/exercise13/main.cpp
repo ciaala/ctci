a49a1cb7ddb1df6f21a779f0141a9488df67cc73
Francesco Fiduccia <francesco.fiduccia@know.eu>
2016-06-21 11:40:15 +0200

    16.13 Completed

--- src/chapter16/exercise13/main.cpp
+++ src/chapter16/exercise13/main.cpp
@@ -19,6 +19,7 @@ namespace exercise16_13 {
         }
 
         friend class Square;
+
         friend class Line;
     };
 
@@ -58,6 +59,28 @@ namespace exercise16_13 {
                 throw std::invalid_argument("Both points are the same");
             }
         }
+
+        Point operator%(Line &line) {
+            // double YY = thils
+            // double XX =
+            double denominator = (a * line.b - b * line.a);
+            if (denominator != 0) {
+                double x;
+                if (line.b != 0) {
+                    x = -(c - (b * line.c) / line.b) * (line.b / denominator);
+                } else if (b != 0) {
+                    x = -(line.c - (line.b * c) / b) * (b / denominator);
+                } else {
+                    throw std::invalid_argument("algorithm cannot proceed");
+                }
+                double y = -(a * x + c) / b;
+                return Point(x, y);
+            } else if (c == line.c) {
+                throw std::invalid_argument("The lines are parallel and adjacent");
+            }
+            throw std::invalid_argument("The lines don't intersect");
+        }
+
         friend ostream &operator<<(ostream &ost, const Line &line) {
             ost << showpos << line.a << "x " << line.b << "y " << line.c;
             return ost;
@@ -65,10 +88,14 @@ namespace exercise16_13 {
     };
 
     void main() {
-        Square sq1 = Square({20,20}, {50, 50});
-        Square sq2 = Square({2000,10}, {2030,900});
-
-        cout << Line::create(sq1.getCenter(), sq2.getCenter()) << endl;
+        Square sq1 = Square({20, 20}, {50, 50});
+        Square sq2 = Square({2000, 10}, {2030, 900});
+        Line diag = Line::create(sq1.getCenter(), sq2.getCenter());
+        // TODO intersect with the perimeter of the square
+        // TODO check condition to find the side of the sq1 and sq2 which are not facing
+        // TODO compare the slope of the diagonals vs slope of the center-line
+        // TODO to be discussed ...
+        cout << diag << endl;
 
     }
 
