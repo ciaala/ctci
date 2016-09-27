c4c57c5531aa3b2b707db72b902872e226379745
ciaala <francesco.fiduccia@gmail.com>
2016-09-27 08:25:39 +0200

    Refactor the LinkedList template methods into a template class ListDecorator

--- src/chapter2/exercise2/main.cpp
+++ src/chapter2/exercise2/main.cpp
@@ -6,17 +6,37 @@
 #include "library/ListDecorator.h"
 
 using namespace std;
-namespace exercise2_1 {
+namespace exercise2_2 {
 
     template<typename T>
     class ReturnKthLast : public ListDecorator<T> {
-
+    private:
+        int kth_last;
     public:
-        ReturnKthLast() : ListDecorator<T>("returnKthLast") {}
+        ReturnKthLast(int kth_last) : ListDecorator<T>("returnKthLast") {
+            this->kth_last = kth_last;
+        }
 
         virtual void execute(list<T> &container) {
+            int count = 0;
+            auto kth_it = container.cend();
+            for (auto it = container.cbegin();
+                 it != container.cend();
+                 it++) {
 
+                if (count <= this->kth_last) {
+                    count++;
+
+                    kth_it = container.cbegin();
+                } else {
+                    kth_it++;
+                }
+            }
+            T d = *kth_it;
+            container.clear();
+            container.push_back(d);
         }
+
 
     };
 
@@ -30,7 +50,7 @@ namespace exercise2_1 {
         intList.emplace_back(1);
         intList.emplace_back(3);
         intList.emplace_back(5);
-        ReturnKthLast<int> runner;
+        ReturnKthLast<int> runner(2);
 
         runner.run(intList);
 
--- src/chapter2/main.cpp
+++ src/chapter2/main.cpp
@@ -16,7 +16,8 @@ namespace exercise2_9 { void main(); };
 
 int main_chapter2(const int argc, const char **argv) {
     exercise2_1::main();
-    /*exercise2_2::main();
+    exercise2_2::main();
+    /*
     exercise2_3::main();
     exercise2_4::main();
     exercise2_5::main();
